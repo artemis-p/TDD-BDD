942ce44c98877fb30504f9f117332f8ff1d0ed33
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-07-01 15:44:38 +0100

    Refactored the test by using hooks - test pass

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -20,3 +20,28 @@ Sandwich = Struct.new(:taste, :toppings)
       expect(toppings).not_to be_empty 
     end 
   end
+
+
+  # Same example using hooks to avoid repetitive set up
+  Sandwich = Struct.new(:taste, :toppings)
+
+  describe 'An ideal sandwich' do 
+    before { @sandwich = Sandwich.new('delicious', []) }
+    it 'is delicious' do
+
+      taste = @sandwich.taste 
+
+      expect(taste).to eq('delicious') 
+    end
+
+    it "let's me add toppings" do  
+      @sandwich.toppings << 'cheese'
+      toppings = @sandwich.toppings
+
+      expect(toppings).not_to be_empty 
+    end 
+  end
+
+
+  
+  
