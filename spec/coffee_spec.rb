c8779ae6649a92ab35a625ce4307bd296926b3c5
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-07-03 16:04:02 +0100

    Running specific failures and how to add configuration for failures

--- spec/coffee_spec.rb
+++ spec/coffee_spec.rb
@@ -1,20 +1,30 @@
-# require 'coffee'
+require 'coffee'
 
-# describe "A cup of coffee" do
-#   let(:coffee) { Coffee.new }
+describe "A cup of coffee" do
+  RSpec.configure do |config|
+    config.example_status_persistence_file_path = 'spec/examples.txt' 
+  end
 
-#   it "costs £1" do
-#     expect(coffee.price).to eq(1.00)
-#   end
+  let(:coffee) { Coffee.new }
 
-#   context "with milk" do
-#     before { coffee.add :milk }
+  it "costs £1" do
+    expect(coffee.price).to eq(1.00)
+  end
 
-#     it "costs £1.25" do
-#       expect(coffee.price).to eq(1.25)
-#     end
-#   end
-# end
+  context "with milk" do
+    before { coffee.add :milk }
+
+    it "costs £1.25" do
+      expect(coffee.price).to eq(1.25)
+    end
+  end
+end
 
 # using rspec -fd => to see the documentation formatter. 
 # using gem install coderay -v 1.1.1 =>the ruby snippets in the spec's output will be color-coded just like they'd be in the text editor
+# rspec -e milk -fd =>runs a specific example by name
+# rspec ./spec/coffee_spec.rb:13 => runs the latest error
+# rspec --only-failures => needs configuration file to store all the information about which examples are failing - add lines 4-5 and then run rspec with no flags
+
+
+
