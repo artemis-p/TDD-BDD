7165b03decdd7b4cf2be5e37402277cc02f84434
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-07-03 16:08:42 +0100

    Running rspec for focused examples

--- spec/coffee_spec.rb
+++ spec/coffee_spec.rb
@@ -2,6 +2,7 @@ require 'coffee'
 
 describe "A cup of coffee" do
   RSpec.configure do |config|
+    config.filter_run_when_matching(focus: true)
     config.example_status_persistence_file_path = 'spec/examples.txt' 
   end
 
@@ -11,7 +12,7 @@ describe "A cup of coffee" do
     expect(coffee.price).to eq(1.00)
   end
 
-  context "with milk" do
+  fcontext "with milk" do # if you add f before context means that it's focused context. configure rspec to run only the focused examples by adding line 5 and run rspec. It will run the focused example
     before { coffee.add :milk }
 
     it "costs £1.25" do
@@ -24,7 +25,7 @@ end
 # using gem install coderay -v 1.1.1 =>the ruby snippets in the spec's output will be color-coded just like they'd be in the text editor
 # rspec -e milk -fd =>runs a specific example by name
 # rspec ./spec/coffee_spec.rb:13 => runs the latest error
-# rspec --only-failures => needs configuration file to store all the information about which examples are failing - add lines 4-5 and then run rspec with no flags
+# rspec --only-failures => needs configuration file to store all the information about which examples are failing - add lines 4+6 and then run rspec with no flags
 
 
 
